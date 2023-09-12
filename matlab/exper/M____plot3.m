290a233716eea5fb894f65715de506e193925814
Adam Seewald <adam@seewald.email>
2023-09-12 18:31:13 -0400

    fixes to the coefficients

--- matlab/exper/M____plot3.m
+++ matlab/exper/M____plot3.m
@@ -2,9 +2,6 @@
 
 fig=figure;
 set(fig,'color','w');
-
-tiledlayout(2,1)
-nexttile
 ax1=gca;
 ax1.YGrid='on';
 ax1.Layer='top';
@@ -36,7 +33,7 @@ for k=1:2:28
     debug2=debug;
 
     maxsize=min([length(debug1.epsilon) length(debug2.epsilon)]);
-    plot(debug0.z(1:maxsize_)/300,.5*debug1.epsilon(:,1:maxsize_)+.5*debug2.epsilon(:,1:maxsize_),...
+    plot(debug0.z(1:maxsize_)/120,.5*debug1.epsilon(:,1:maxsize_)+.5*debug2.epsilon(:,1:maxsize_),...
         'color','r')
     curves=[curves;...
         .5*debug1.epsilon(:,1:maxsize_)+.5*debug2.epsilon(:,1:maxsize_)];
@@ -45,47 +42,26 @@ max_curves=max(curves,[],1);
 min_curves=min(curves,[],1);
 
 ylim([0 .15]);
-xticks([.4 .55 .7 .85 1]);
 
-curve_avg = avgcurve(maxsize_, {[debug0.z(1:maxsize_)/300 curves(1,:)'],...
-    [debug0.z(1:maxsize_)/300 curves(2,:)'],...
-    [debug0.z(1:maxsize_)/300 curves(3,:)'],...
-    [debug0.z(1:maxsize_)/300 curves(4,:)'],...
-    [debug0.z(1:maxsize_)/300 curves(5,:)'],...
-    [debug0.z(1:maxsize_)/300 curves(6,:)'],...
-    [debug0.z(1:maxsize_)/300 curves(7,:)'],...
-    [debug0.z(1:maxsize_)/300 curves(8,:)'],...
-    [debug0.z(1:maxsize_)/300 curves(9,:)'],...
-    [debug0.z(1:maxsize_)/300 curves(10,:)'],...
-    [debug0.z(1:maxsize_)/300 curves(11,:)'],...
-    [debug0.z(1:maxsize_)/300 curves(12,:)'],...
-    [debug0.z(1:maxsize_)/300 curves(13,:)'],...
-    [debug0.z(1:maxsize_)/300 curves(14,:)']});
+curve_avg = avgcurve(maxsize_, {[debug0.z(1:maxsize_)/120 curves(1,:)'],...
+    [debug0.z(1:maxsize_)/120 curves(2,:)'],...
+    [debug0.z(1:maxsize_)/120 curves(3,:)'],...
+    [debug0.z(1:maxsize_)/120 curves(4,:)'],...
+    [debug0.z(1:maxsize_)/120 curves(5,:)'],...
+    [debug0.z(1:maxsize_)/120 curves(6,:)'],...
+    [debug0.z(1:maxsize_)/120 curves(7,:)'],...
+    [debug0.z(1:maxsize_)/120 curves(8,:)'],...
+    [debug0.z(1:maxsize_)/120 curves(9,:)'],...
+    [debug0.z(1:maxsize_)/120 curves(10,:)'],...
+    [debug0.z(1:maxsize_)/120 curves(11,:)'],...
+    [debug0.z(1:maxsize_)/120 curves(12,:)'],...
+    [debug0.z(1:maxsize_)/120 curves(13,:)'],...
+    [debug0.z(1:maxsize_)/120 curves(14,:)']});
 
-xticklabels(ax1,{})
-t.TileSpacing = 'compact';
 
-nexttile
-ax1=gca;
-ax1.YGrid='on';
-ax1.Layer='top';
-ax1.GridLineStyle=':';
-ax1.GridAlpha=.5;
-set(ax1,'xdir','reverse');
-grid on;
-hold on;
-ylim([0 .15]);
 
-x2=[debug0.z(1:maxsize_)/300; flip(debug0.z(1:maxsize_)/300,1)];
-y2=[min_curves'; flip(max_curves',1)];
-hf=fill(x2,y2,'b');
-set(hf,'facealpha',.4)
-
-plot(debug0.z(1:maxsize_)/300,curve_avg(:,2),'color','b','MarkerSize',3)
-xticks([.4 .55 .7 .85 1]);
-
-WIDTH=122;
-HEIGHT=WIDTH;
+WIDTH=132;
+HEIGHT=WIDTH*2/3;
 FIXED_SPACE=220;
 SCREEN_HEIGHT=get(0,'screensize')*.75;
 SCREEN_HEIGHT=SCREEN_HEIGHT(4)-FIXED_SPACE;
@@ -97,4 +73,40 @@ set(fig,'units','points','position',...
      WIDTH,...
      HEIGHT]);
 set(fig,'Renderer','painters');
-saveas(fig,strcat('data/','fig_erg-soc'),'svg');
+saveas(fig,strcat('data/','fig_erg-soc_1'),'svg');
+
+
+fig2=figure;
+set(fig2,'color','w');
+
+ax1=gca;
+ax1.YGrid='on';
+ax1.Layer='top';
+ax1.GridLineStyle=':';
+ax1.GridAlpha=.5;
+set(ax1,'xdir','reverse');
+grid on;
+hold on;
+ylim([0 .15]);
+
+x2=[debug0.z(1:maxsize_)/120; flip(debug0.z(1:maxsize_)/120,1)];
+y2=[min_curves'; flip(max_curves',1)];
+hf=fill(x2,y2,'b');
+set(hf,'facealpha',.4)
+
+plot(debug0.z(1:maxsize_)/120,curve_avg(:,2),'color','b','MarkerSize',3)
+
+WIDTH=132;
+HEIGHT=WIDTH/3;
+FIXED_SPACE=220;
+SCREEN_HEIGHT=get(0,'screensize')*.75;
+SCREEN_HEIGHT=SCREEN_HEIGHT(4)-FIXED_SPACE;
+
+pause(.5);
+set(fig2,'units','points','position',...
+    [FIXED_SPACE+mod(1,6)*(WIDTH+35),...
+     SCREEN_HEIGHT-floor(1/6)*(HEIGHT+110),...
+     WIDTH,...
+     HEIGHT]);
+set(fig2,'Renderer','painters');
+saveas(fig2,strcat('data/','fig_erg-soc_2'),'svg');
